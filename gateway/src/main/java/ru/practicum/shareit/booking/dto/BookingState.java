845086664655f107d844ac5ed6c2185bf388c800
Viktor64 <ecogorod64@mail.ru>
2025-04-22 21:14:10 +0400

    Доработал ветку: add-item-requests-and-gateway. 1) Удалил не используемый класс BookingStateValidator. 2) Переписал метод from в классе BookingState с использованием Stream API. 3) Заменил Map<String, String> в ErrorHandler на отдельный класс ErrorResponse. 4) Заменил e на более подходящую exception.

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingState.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingState.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.booking.dto;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public enum BookingState {
@@ -11,11 +12,8 @@ public enum BookingState {
     REJECTED;
 
     public static Optional<BookingState> from(String stringState) {
-        for (BookingState state : values()) {
-            if (state.name().equalsIgnoreCase(stringState)) {
-                return Optional.of(state);
-            }
-        }
-        return Optional.empty();
+        return Arrays.stream(values())
+                .filter(state -> state.name().equalsIgnoreCase(stringState))
+                .findFirst();
     }
 }
--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,33 +8,53 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationException(final MethodArgumentNotValidException e) {
-        log.error("Ошибка валидации: {}", e.getMessage());
-        return Map.of("error", "Ошибка валидации",
-                "message", e.getBindingResult().getFieldError().getDefaultMessage());
+    public ErrorResponse handleValidationException(final MethodArgumentNotValidException exception,
+                                                   HttpServletRequest request) {
+        log.error("Ошибка валидации: {}", exception.getMessage());
+        return ErrorResponse.builder()
+                .error("Ошибка валидации")
+                .message(exception.getBindingResult().getFieldError().getDefaultMessage())
+                .timestamp(LocalDateTime.now().format(FORMATTER))
+                .path(request.getRequestURI())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleIllegalArgumentException(final IllegalArgumentException e) {
-        log.error("Ошибка в аргументах запроса: {}", e.getMessage());
-        return Map.of("error", "Ошибка в аргументах запроса",
-                "message", e.getMessage());
+    public ErrorResponse handleIllegalArgumentException(final IllegalArgumentException exception,
+                                                        HttpServletRequest request) {
+        log.error("Ошибка в аргументах запроса: {}", exception.getMessage());
+        return ErrorResponse.builder()
+                .error("Ошибка в аргументах запроса")
+                .message(exception.getMessage())
+                .timestamp(LocalDateTime.now().format(FORMATTER))
+                .path(request.getRequestURI())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleException(final Exception e) {
-        log.error("Непредвиденная ошибка: {}", e.getMessage(), e);
-        return Map.of("error", "Непредвиденная ошибка",
-                "message", e.getMessage());
+    public ErrorResponse handleException(final Exception exception,
+                                         HttpServletRequest request) {
+        log.error("Непредвиденная ошибка: {}", exception.getMessage(), exception);
+        return ErrorResponse.builder()
+                .error("Непредвиденная ошибка")
+                .message(exception.getMessage())
+                .timestamp(LocalDateTime.now().format(FORMATTER))
+                .path(request.getRequestURI())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
     }
 }
